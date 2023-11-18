ad5f4cded3082a5749a5459494d7747674e39de4
Noah Sylvester <148350695+DoktorVati@users.noreply.github.com>
2023-11-18 15:22:32 -0500

    Update Tower.java

--- BioBattle/app/src/main/java/com/biobattle/Tower.java
+++ BioBattle/app/src/main/java/com/biobattle/Tower.java
@@ -3,30 +3,67 @@ package com.biobattle;
 import android.widget.ImageView;
 
 public class Tower {
+    private int towerNumber;
     private ImageView imageView;
-    public float attackDamage = 10f;
-    public float attackSpeed = 1f;
-    public float attackRange = 5f;
-    public float upgradePercentage = 0.75f;
+    private float attackDamage;
+    private float attackSpeed;
+    private float attackRange;
+    private float upgradePercentage = 1.15f;
     private int totalUpgrades = 0;
-
-    public Tower(ImageView imageView) {
+    private AttackRangeView attackRangeView;
+    private float newAttackRange, newAttackDamage, newAttackSpeed;
+    public Tower(ImageView imageView, float attackRange, float attackDamage, float attackSpeed)
+    {
         this.imageView = imageView;
+        this.attackRange = attackRange;
+        this.attackDamage = attackDamage;
+        this.attackSpeed = attackSpeed;
+    }
+    public void setTowerNumber(int towerNumber) {
+        this.towerNumber = towerNumber;
     }
 
-    public void upgrade() {
-        float damageMultiplier = 1 + totalUpgrades * upgradePercentage;
-        float speedMultiplier = 1 + totalUpgrades;
-        float rangeMultiplier = 1 + totalUpgrades * (upgradePercentage / 2);
+    public void upgrade(int imageResource) {
+        newAttackRange = attackRange * upgradePercentage;
+        newAttackDamage = attackDamage * upgradePercentage;
+        newAttackSpeed = attackSpeed * upgradePercentage;
 
-        attackDamage *= damageMultiplier;
-        attackSpeed *= speedMultiplier;
-        attackRange *= rangeMultiplier;
-
-        totalUpgrades += 1;
+        attackRange = (newAttackRange *= upgradePercentage);
+        attackDamage = (newAttackDamage *= upgradePercentage);
+        attackSpeed = (newAttackSpeed *= upgradePercentage);
     }
-
-    public ImageView getImageView() {
+    public ImageView getImageView()
+    {
         return imageView;
+    }
+    public float getAttackRange()
+    {
+        return this.attackRange;
+    }
+    public void setAttackRange(float attackRange)
+    {
+        this.attackRange = attackRange;
+    }
+    public float getDamage()
+    {
+        return this.attackDamage;
+    }
+    public void setDamage(float attackDamage)
+    {
+        this.attackDamage = attackDamage;
+    }
+    public float getAttackSpeed()
+    {
+        return attackSpeed;
+    }
+    public void setAttackSpeed(float attackSpeed)
+    {
+        this.attackSpeed = attackSpeed;
+    }
+
+    //optional in case we want to have a limit on upgrades, currently Infinite
+    public int getTotalUpgrades()
+    {
+        return totalUpgrades;
     }
 }
