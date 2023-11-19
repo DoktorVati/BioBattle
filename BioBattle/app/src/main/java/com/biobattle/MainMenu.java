9fb8cda526d08e68a7e8f7750536ce75be4d7af9
Noah Sylvester <148350695+DoktorVati@users.noreply.github.com>
2023-11-19 15:57:43 -0500

    Update MainMenu.java
    Added Music! And Life!

--- BioBattle/app/src/main/java/com/biobattle/MainMenu.java
+++ BioBattle/app/src/main/java/com/biobattle/MainMenu.java
@@ -1,22 +1,79 @@
-package com.biobattle;
-import androidx.appcompat.app.AppCompatActivity;
-import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.content.Intent;
+<RelativeLayout
+    xmlns:android="http://schemas.android.com/apk/res/android"
+    xmlns:tools="http://schemas.android.com/tools"
+    android:layout_width="match_parent"
+    android:layout_height="match_parent"
+    android:backgroundTintMode="src_atop"
+    android:backgroundTint="@color/invisible"
+    android:background="@drawable/main_menu_background">
 
-import com.biobattle.MainActivity;
+    <ImageView
+        android:id ="@+id/mainMenu"
+        android:layout_width="wrap_content"
+        android:layout_height="320dp"
+        android:layout_alignParentTop="false"
+        android:contentDescription="@string/menuContentDescription"
+        android:src="@drawable/bio_battle_title" />
 
-public class MainMenu extends AppCompatActivity {
+    <ImageButton
+        android:id="@+id/playButton"
+        android:layout_width="wrap_content"
+        android:layout_height="wrap_content"
+        android:layout_below="@id/mainMenu"
+        android:layout_centerHorizontal="true"
+        android:layout_marginTop="-100dp"
+        android:layout_marginBottom="40dp"
+        android:text="@string/play_text"
+        android:onClick="playGame"
+        android:src="@drawable/playbutton1"
+        android:background="@color/invisible"
+        android:scaleY="0.5"
+        android:scaleX="0.5"
+        android:contentDescription="@string/play_text"
+        />
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_menu);
-    }
+    <ImageView
+        android:layout_width="wrap_content"
+        android:layout_height="wrap_content"
+        android:src="@drawable/simpletower"
+        android:layout_alignParentEnd="true"
+        android:layout_alignParentBottom="true"
+        android:layout_marginEnd="150dp"
+        android:layout_marginBottom="255dp" />
 
-    public void playGame(View view) {
-        Intent intent = new Intent(this, MainActivity.class);
-        startActivity(intent);
-    }
-}
+    <ImageView
+        android:layout_width="wrap_content"
+        android:layout_height="wrap_content"
+        android:src="@drawable/golgitower"
+        android:layout_alignParentStart="true"
+        android:layout_alignParentBottom="true"
+        android:layout_marginStart="150dp"
+        android:layout_marginBottom="255dp"
+        android:scaleType="center"
+        android:scaleY="2"
+        android:scaleX="2"/>
+    <ImageView
+        android:layout_width="wrap_content"
+        android:layout_height="wrap_content"
+        android:src="@drawable/killeridleanim"
+        android:layout_alignParentEnd="true"
+        android:layout_alignParentBottom="true"
+        android:layout_marginEnd="130dp"
+        android:layout_marginBottom="90dp"
+        android:scaleX="1.2"
+        android:scaleType="center"
+        android:scaleY = "1.2"/>
+
+    <ImageView
+        android:layout_width="wrap_content"
+        android:layout_height="wrap_content"
+        android:src="@drawable/cannontower"
+        android:layout_alignParentStart="true"
+        android:layout_alignParentBottom="true"
+        android:layout_marginStart="150dp"
+        android:layout_marginBottom="105dp"
+        android:scaleType="center"
+        android:scaleY="1.2"
+        android:scaleX="1.2"/>
+    <!-- Add other menu items as needed -->
+</RelativeLayout>
